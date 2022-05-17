08e62c46ed662501da40a9e58e940dafbc83de0a
Moxf94 <moxf94@gmail.com>
2022-05-18 00:17:08 +0700

    first

---
+++ Tower Defence/Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CubeEditor : MonoBehaviour
+{
+
+
+    [SerializeField] [Range(1f,20f)] private float gridSize = 10f;
+
+    TextMesh label;
+    void Update()
+    {
+        Vector3 snaPos;
+
+        snaPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snaPos.y = 0f;
+        snaPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+        transform.position = snaPos;
+
+        label = GetComponentInChildren<TextMesh>();
+        string labelName = snaPos.x / gridSize + "," + snaPos.z / gridSize;
+        label.text = labelName;
+        gameObject.name = labelName;
+
+    }
+}
---
+++ Tower Defence/Assets/Scripts/EnemyMoviement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMoviement : MonoBehaviour
+{
+   [SerializeField] List<WayPoint> blocksList;    
+
+
+    void Start()
+    {
+        PrintWayPointName();
+        StartCoroutine(PrintWayPointName());
+    }
+
+
+    IEnumerator PrintWayPointName()
+    {
+        foreach (WayPoint waypoint in blocksList)
+        {
+            print("Враг начал движение");
+            transform.position = waypoint.transform.position;
+            print("Враг пришел на позицию : " + waypoint);
+            Debug.Log(waypoint.name);
+
+            yield return new WaitForSeconds(1f);
+        }
+        print("Враг закончил движение");
+    }
+}
